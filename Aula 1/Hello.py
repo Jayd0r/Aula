20484b074274aa7cab27db29b9eef318de03dba8
Jayd0r <jayd0r.vascalves6@gmail.com>
2019-04-09 12:56:18 +0100

    Jogo toques

---
+++ Aula 1/Hello.py
@@ -0,0 +1,19 @@
+
+def average(num_list):
+    total = sum(average(num_list))
+    num != 0:
+        average = total / num_list
+    print(average)
+    else:
+        print("No scores")
+    
+
+def average(num_list):
+    total = sum(num_list)
+    num != 0:
+        average = total / num_list
+    print(average)
+    else:
+        print("No scores")
+
+average([1, 2, 3, 4, 5, 6])
